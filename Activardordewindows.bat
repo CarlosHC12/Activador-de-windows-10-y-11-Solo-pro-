734d3099878fa9d4d55d667a63eee237a9b55e30
CarlosHC12 <carloshurtadocortes@gmail.com>
2025-01-14 16:23:43 +0100

    Add files via upload

---
+++ Activardordewindows.bat
@@ -0,0 +1,471 @@
+
+@echo off
+color 30
+title Programa de activacion de windows By CarlosHC
+@echo off
+winver
+echo   ==================================
+echo   =    Programa de activacion      =
+echo   =  Recuerde ejecutar el archivo  =
+echo   =       como administrador       =
+echo   ==================================
+echo         Scrip creado por CarlosHC
+echo   Para seleccionar escriba el numero y presione enter
+
+
+
+set /p var= Seleccione 1 para Windows 10, 2 para Windows 11, 3 o enter vacio sales del activador:
+
+
+if %var%==1 goto :WIN10
+if %var%==2 goto :WIN11
+if %var%==3 goto exit
+if %var% GTR 3 echo Error
+goto :Menu
+
+:WIN10
+title Programa de activacion de windows By CarlosHC Windows 10
+echo Escoja su edicion con los numeros indicados. Algunos te daran 2 opciones, si una no funciona prueba con la otra.
+echo 10PRO (1)
+echo 10PROEDU (2)
+echo 10PROEDUN (3)
+echo 10PRON (4)
+echo 10PROSERIAL (5)
+echo 10ENTERPRISE (6)
+echo 10ENTERPRISEG (7)
+echo 10ENTERPRISEGN (8)
+echo 10ENTERPRISEN (9)
+echo 10HOME (10)
+echo 10HOMESINGLELANG (11)
+echo 10EDUCA (12)
+echo 10EDUCAN (13)
+set /p var= 
+
+if %var%==1 goto :PRO
+if %var%==2 goto :PROEDU
+if %var%==3 goto :PROEDUN
+if %var%==4 goto :PRON
+if %var%==5 goto :PROSERIA
+if %var%==6 goto :ENTERPRISE
+if %var%==7 goto :ENTERPRISEG
+if %var%==8 goto :ENTERPRISEGN
+if %var%==9 goto :ENTERPRISEN
+if %var%==10 goto :HOME
+if %var%==11 goto :HOMESINGLELANG
+if %var%==12 goto :EDUCA
+if %var%==13 goto :EDUCAN
+
+
+:PRO
+set /p var= Seleccione 1 o 2 
+if %var%==1 goto :PRO1
+if %var%==2 goto :PRO2
+
+:PRO1
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk VK7JG-NPHTM-C97JM-9MPGT-3V66T
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:PRO2
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:PROEDU
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk 6TP4R-GNPTD-KYYHQ-7B7DP-J447Y
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:PROEDUN
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk  YVWGF-BXNMC-HTQYQ-CPQ99-66QFC
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+
+:PRON
+set /p var= Seleccione 1 o 2 
+if %var%==1 goto :PRON1
+if %var%==2 goto :PRON2
+
+:PRON1
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:PRON2
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk 9FNHH-K3HBT-3W4TD-6383H-6XYWF
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:PROSERIA
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk  W269N-WFGWX-YVC9B-4J6C9-T83GX
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:ENTERPRISE
+set /p var= Seleccione 1 o 2 
+if %var%==1 goto :ENTER1
+if %var%==2 goto :ENTER2
+
+:ENTER1
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:ENTER2
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk NPPR9-FWDCX-D2C8J-H872K-2YT43
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:ENTERPRISEG
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk  YYVX9-NTFWV-6MDM3-9PT4T-4M68B
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+
+:ENTERPRISEGN
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk  44RPN-FTY23-9VTTB-MP9BX-T84FV
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:ENTERPRISEN
+
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk  DPH2V-TTNVB-4X9Q3-TJR4H-KHJW4
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:HOME
+
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk  TX9XD-98N7V-6WMQ6-BX7FG-H8Q99
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+
+:HOMESINGLELANG
+
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk  7HNRX-D7KGG-3K4RQ-4WPJ4-YTDFH
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+
+:EDUCA
+
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk  NW6C2-QMPVW-D7KKK-3GKT6-VCFB2
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+
+:EDUCAN
+
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk  2WH4N-8QGBV-H22JP-CT43Q-MDWWJ
+
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+
+
+
+
+
+
+
+@echo off
+
+:WIN11
+echo Introduzca el numero correspondiente a la version que tienes instalada 1=PRO 2=HOME
+title Programa de activacion de windows By CarlosHC Windows 11
+
+set /p var= Seleccione edicion
+if %var%==1 goto :Pro11
+if %var%==2 goto :HOME11
+
+:Pro11
+echo esta edicion tiene varias claves de activacion, elija una clave y si una le da error, pruebe con la siguiente.
+
+set /p var= Seleccione clave escriba un numero del 1 al 11. Recuerde comprbar si se acivo despues de usar el programa, si no lo esta prueba con la siguente clave.
+if %var%==1 goto :11PROA
+if %var%==2 goto :11PROB
+if %var%==3 goto :11PROC
+if %var%==4 goto :11PROD
+if %var%==5 goto :11PROE
+if %var%==6 goto :11PROF
+if %var%==7 goto :11PROG
+if %var%==8 goto :11PROH
+if %var%==9 goto :11PROI
+if %var%==10 goto :11PROJ
+if %var%==11 goto :11PROK
+
+
+
+:11PROA
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk W269N-WFGWX-YVC9B-4J6C9-T83GX
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:11PROB
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk VK7JG-NPHTM-C97JM-9MPGT-3V66T
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:11PROC
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:11PROD
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk NRG8B-VKK3Q-CXVCJ-9G2XF-6Q84J
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:11PROE
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk 2B87N-8KFHP-DKV6R-Y2C8J-PKCKT
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:11PROF
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk A269N-WFGWX-YVC9B-4J6C9-T83GX
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:11PROG
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk A269N-WFGWX-YVC9B-4J6C9-T83GX
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:11PROH
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk Q269N-WFGWX-YVC9B-4J6C9-T83GX
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exi
+
+:11PROI
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk 9FNHH-K3HBT-3W4TD-6383H-6XYWF
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:11PROJ
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk 6TP4R-GNPTD-KYYHQ-7B7DP-J447Y
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+:11PROK
+echo Activando windows, pulse "Aceptar" en las ventanas emergentes 
+
+slmgr /ipk MH37W-N47XK-V7XM9-C7227-GCQG9
+
+slmgr /skms kms.digiboy.ir
+
+slmgr /ato
+
+echo Fin de la activacion, si salio un mensaje de activacion correcta no es necesario nada mas.
+pause
+exit
+
+Pause>Nul
